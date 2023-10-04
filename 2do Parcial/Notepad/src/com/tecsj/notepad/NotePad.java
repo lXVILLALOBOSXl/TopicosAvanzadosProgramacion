b8d6a0065b6c4fbe0d1edcd1ebc0f755c96fd2d9
Luis Villalobos <ivillalobosrivera@yahoo.com>
2023-10-03 19:32:10 -0600

    NotePad V2

--- 2do Parcial/Notepad/src/com/tecsj/notepad/NotePad.java
+++ 2do Parcial/Notepad/src/com/tecsj/notepad/NotePad.java
@@ -1,6 +1,8 @@
 package com.tecsj.notepad;
+
 import java.io.*;
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -9,10 +11,12 @@ import static com.tecsj.util.GUI.createMenuItem;
 
 public class NotePad extends JFrame implements ActionListener, WindowListener{
 
-    JTextArea jta = new JTextArea();
+    JTextPane jtp = new JTextPane();
+    StyledDocument styledDocument = new DefaultStyledDocument();
     File fnameContainer;
 
     public NotePad(){
+        jtp.setStyledDocument(styledDocument);
         Font fnt = new Font("Arial",Font.PLAIN,15);
         Container frame = getContentPane();
         JMenuBar jmb = new JMenuBar();
@@ -23,13 +27,15 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
         JMenu jmhelp = new JMenu("Help");
 
         frame.setLayout(new BorderLayout());
-        JScrollPane sbrText = new JScrollPane(jta);
+        JScrollPane sbrText = new JScrollPane(jtp);
         sbrText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         sbrText.setVisible(true);
 
-        jta.setFont(fnt);
-        jta.setLineWrap(true);
-        jta.setWrapStyleWord(true);
+        jtp.setFont(fnt);
+        // Enable line wrapping
+        //jtp.putClientProperty(JTextPane.W3C_LENGTH_UNITS, true);
+
+
 
         add(sbrText);
 
@@ -86,7 +92,7 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
 
         // Add components to the frame using layout managers
         frame.add(jtb, BorderLayout.NORTH);
-        frame.add(new JScrollPane(jta), BorderLayout.CENTER);
+        frame.add(new JScrollPane(jtp), BorderLayout.CENTER);
 
         setIconImage(Toolkit.getDefaultToolkit().getImage("notepad.gif"));
         addWindowListener(this);
@@ -100,7 +106,7 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
         JFileChooser jfc = new JFileChooser();
         if(e.getActionCommand().equals("New")){
             this.setTitle("UntitledNew.txt - Notepad");
-            jta.setText("");
+            jtp.setText("");
             fnameContainer = null;
         }else if (e.getActionCommand().equals("Open")){
             int ret = jfc.showDialog(null,"Open");
@@ -112,7 +118,7 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
                     fnameContainer = fyl;
                 }catch (IOException ers){}
             }
-        } else if (e.getActionCommand().equals("Save")) {
+        } else if (e.getActionCommand().equals("Save as ...")) {
             if(fnameContainer != null){
                 jfc.setCurrentDirectory(fnameContainer);
                 jfc.setSelectedFile(fnameContainer);
@@ -130,16 +136,125 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
                     fnameContainer = fyl;
                 }catch (Exception ets){}
             }
+        }else if (e.getActionCommand().equals("Save")) {
+            if (fnameContainer != null) {
+                try {
+                    // Check if the file exists, if so, save directly
+                    SaveFile(fnameContainer.getAbsolutePath());
+                    this.setTitle(fnameContainer.getName() + " - Notepad");
+                } catch (Exception ex) {
+                    // Handle exceptions (e.g., file not found or unable to save)
+                    ex.printStackTrace();
+                }
+            } else {
+                // If fnameContainer is null, this is equivalent to "Save As..."
+                // Implement "Save As..." functionality here
+                // You can use the existing code for "Save As..." with appropriate modifications
+                if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        File file = jfc.getSelectedFile();
+                        SaveFile(file.getAbsolutePath());
+                        this.setTitle(file.getName() + " - Notepad");
+                        fnameContainer = file;
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
         }else if (e.getActionCommand().equals("Exit")){
             Exiting();
         }else if (e.getActionCommand().equals("Copy")){
-            jta.copy();
+            jtp.copy();
         }else if (e.getActionCommand().equals("Paste")){
-            jta.paste();
-        }else if (e.getActionCommand().equals("About Notepad")){
+            jtp.paste();
+        }else if (e.getActionCommand().equals("Find")) {
+            // Create a dialog to input the search text
+            String searchText = JOptionPane.showInputDialog("Find:");
+
+            if (searchText != null) {
+                String text = jtp.getText();
+                int startIndex = text.indexOf(searchText);
+
+                if (startIndex != -1) {
+                    // Text was found, select and scroll to the found text
+                    jtp.select(startIndex, startIndex + searchText.length());
+                    jtp.setCaretPosition(startIndex);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Text not found.");
+                }
+            }
+        }else if (e.getActionCommand().equals("Font")) {
+            // Create an array of available font names
+            String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+            // Prompt the user to select a font
+            String selectedFont = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Choose a font:",
+                    "Font Selection",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    fontNames,
+                    fontNames[0]);
+
+            if (selectedFont != null) {
+                // Prompt the user to select a font size
+                String fontSizeString = JOptionPane.showInputDialog("Enter font size:");
+                int fontSize = Integer.parseInt(fontSizeString);
+
+                // Apply the selected font and size to the text in the JTextArea
+                Font newFont = new Font(selectedFont, Font.PLAIN, fontSize);
+                jtp.setFont(newFont);
+            }
+        }else if (e.getActionCommand().equals("Background")) {
+            // Show a color chooser dialog
+            Color selectedColor = JColorChooser.showDialog(null, "Choose Background Color", jtp.getBackground());
+
+            if (selectedColor != null) {
+                // Set the selected color as the background color for the JTextArea
+                jtp.setBackground(selectedColor);
+            }
+        }else if (e.getActionCommand().equals("Adjust")) {
+            // Define alignment options
+            String[] alignmentOptions = { "Right", "Left", "Center" };
+
+            // Prompt the user to select an alignment
+            String selectedAlignment = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Choose text alignment:",
+                    "Text Alignment",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    alignmentOptions,
+                    alignmentOptions[0]);
+
+            if (selectedAlignment != null) {
+                int alignment = SwingConstants.LEFT; // Default to left alignment
+
+                // Determine the chosen alignment
+                if (selectedAlignment.equals("Left")) {
+                    alignment = SwingConstants.CENTER;
+                } else if (selectedAlignment.equals("Center")) {
+                    alignment = SwingConstants.RIGHT;
+                }
+
+                // Apply the chosen alignment to the selected text in the JTextArea
+                StyledDocument doc = jtp.getStyledDocument();
+                int start = jtp.getSelectionStart();
+                int end = jtp.getSelectionEnd();
+
+                // Create a SimpleAttributeSet to hold the alignment style
+                SimpleAttributeSet alignmentStyle = new SimpleAttributeSet();
+                StyleConstants.setAlignment(alignmentStyle, alignment);
+
+                // Apply the alignment style to the selected text
+                doc.setParagraphAttributes(start, end - start, alignmentStyle, false);
+            }
+        }
+        else if (e.getActionCommand().equals("About Notepad")){
             JOptionPane.showMessageDialog(this,"Created by : Luis Villalobos","Notepad",JOptionPane.INFORMATION_MESSAGE);
         }else if (e.getActionCommand().equals("Cut")){
-            jta.cut();
+            jtp.cut();
         }
 
     }
@@ -147,10 +262,10 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
     public void OpenFile(String fname) throws IOException{
         BufferedReader d = new BufferedReader(new InputStreamReader(new FileInputStream(fname)));
         String l;
-        jta.setText("");
+        jtp.setText("");
         setCursor(new Cursor(Cursor.WAIT_CURSOR));
         while ((l = d.readLine()) != null){
-            jta.setText(jta.getText() + l + "\r\n");
+            jtp.setText(jtp.getText() + l + "\r\n");
         }
         setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
         d.close();
@@ -159,7 +274,7 @@ public class NotePad extends JFrame implements ActionListener, WindowListener{
     public void SaveFile(String fname) throws IOException{
         setCursor(new Cursor(Cursor.WAIT_CURSOR));
         DataOutputStream o = new DataOutputStream(new FileOutputStream(fname));
-        o.writeBytes(jta.getText());
+        o.writeBytes(jtp.getText());
         o.close();
         setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
